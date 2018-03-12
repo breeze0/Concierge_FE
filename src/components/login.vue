3d6392951ef16acb37a54503823e4b1bbe7cc251
breeze0 <13402878236@163.com>
2018-03-12 10:35:47 +0800

    Init

---
+++ src/components/login.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="login-container">
+    <el-card class="box-card">
+      <div class="login-content">
+        hello world
+      </div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped lang="scss">
+.login-container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+
+
+  .box-card {
+    width: 480px;
+  }
+
+  .login-content {
+
+  }
+}
+</style>
