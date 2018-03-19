1605f184e566199e2b6db3e1bc7cf340c840ca87
breeze0 <13402878236@163.com>
2018-03-19 15:34:45 +0800

    Add tabs and card in admin component

--- src/components/admin.vue
+++ src/components/admin.vue
@@ -1,11 +1,39 @@
 <template>
-  <div>
-    hello world
+  <div class="admin-com-container">
+    <div class="tabs-wrapper">
+      <el-tabs v-model="activeName" @tab-click="handleClick">
+        <el-tab-pane label="我拥有的预约项目" name="first">
+          <div class="card-wrapper">
+            <div class="card-item add-card">
+              <el-card class="box-card">
+                <div class="card-content">
+                  <div class="card-icon">
+                    <i class="el-icon-circle-plus"></i>
+                  </div>
+                  <p>创建新的预约项目</p>
+                </div>
+              </el-card>
+            </div>
+          </div>
+        </el-tab-pane>
+        <el-tab-pane label="我参与的预约项目" name="second">hello world2</el-tab-pane>
+        <el-tab-pane label="我拥有的分类" name="third">hello world3</el-tab-pane>
+      </el-tabs>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
-
+    data() {
+      return {
+        activeName: 'first'
+      };
+    },
+    methods: {
+      handleClick(tab, event) {
+        console.log(tab, event);
+      }
+    }
   }
 </script>
