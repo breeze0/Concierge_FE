9f99165efeb7018de0ed9200d9774cc5b0a747a0
breeze0 <13402878236@163.com>
2018-04-10 11:51:14 +0800

    Modify regular for telephone test

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -50,7 +50,7 @@
     },
     methods: {
       isAvailable(val) {
-        var reg=/^[1][3,4,5,7,8][0-9]{9}$/;
+        var reg=/^1[3|4|5|7|8][0-9]{9}$/;
         if (!reg.test(val)) {
           return false;
         } else {
